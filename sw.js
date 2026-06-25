a9a4c5dfc3ecc0d341ca61154f7c240519cb8e18
BraeMonk <160609439+BraeMonk@users.noreply.github.com>
2026-06-25 13:11:28 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    RELAY — MIDI Controller PWA  ·  sw.js
    ───────────────────────────────────────────── */
 
-const CACHE = 'relay-v17';
+const CACHE = 'relay-v18';
 
 const PRECACHE = [
   './',
