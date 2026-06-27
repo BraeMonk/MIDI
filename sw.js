8876e743a7d3a14afffad770d993b5d88de15626
BraeMonk <160609439+BraeMonk@users.noreply.github.com>
2026-06-26 21:25:15 -0400

    Update service worker cache version to v26

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    RELAY — MIDI Controller PWA  ·  sw.js
    ───────────────────────────────────────────── */
 
-const CACHE = 'relay-v25';
+const CACHE = 'relay-v26';
 
 const PRECACHE = [
   './',
