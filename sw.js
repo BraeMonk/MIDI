1a8b0c6a1e652b795321cf1e06488ee41f508410
BraeMonk <160609439+BraeMonk@users.noreply.github.com>
2026-06-27 10:23:29 -0400

    Update service worker cache version to v31

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    RELAY — MIDI Controller PWA  ·  sw.js
    ───────────────────────────────────────────── */
 
-const CACHE = 'relay-v30';
+const CACHE = 'relay-v31';
 
 const PRECACHE = [
   './',
